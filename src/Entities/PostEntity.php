02f85369a9091429ec729d351ed95d966683f801
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-03-08 00:14:16 +0000

    Create post entity

--- src/Entities/PostEntity.php
+++ src/Entities/PostEntity.php
@@ -2,5 +2,58 @@
 
 class PostEntity
 {
-    private 
+    private int $id;
+    private string $title;
+    private ?string $author_name;
+    private string $content;
+    private string $created_at;
+    private int $category_id;
+    private int $likes;
+    private int $dislikes;
+    private string $category;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getAuthorName(): ?string
+    {
+        return $this->author_name;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->created_at;
+    }
+
+    public function getCategoryId(): int
+    {
+        return $this->category_id;
+    }
+
+    public function getLikes(): int
+    {
+        return $this->likes;
+    }
+
+    public function getDislikes(): int
+    {
+        return $this->dislikes;
+    }
+
+    public function getCategory(): string
+    {
+        return $this->category;
+    }
 }
